859460b6bf343a7f21caa60caaf0de3e8fd59775
kunalP2307 <kunalpatil2307@gmail.com>
2022-12-26 22:11:16 +0530

    ternary operator

--- src/ShortCircuitEval.js
+++ src/ShortCircuitEval.js
@@ -3,6 +3,8 @@ import React,{useState, useEffect} from "react";
 const ShortCircuit =()=>{
     
     const[text, setText] = useState('Blockchain');
+    const[isError, setError] = useState(false);
+    
     const firstVal = text || "Some text";
     const secondVal = text && "Some other text";
 
@@ -10,9 +12,14 @@ const ShortCircuit =()=>{
         <>
             {/* <h2>{firstVal}</h2>
             <h2>value : {secondVal}</h2> */}
-            <h1> {text || 'Or'}</h1>
-            <h1> {text && 'And'}</h1>
-            <h1> {!text && 'not'}</h1>
+            {/* <h1> {text || 'Or'}</h1> */}
+            <button onClick={
+                ()=>setError(!isError)
+            }>Toggle Error</button>
+            
+            <h1> {isError && 'And'}</h1>
+            <h1> {isError ? 'if true' : 'if false' }</h1>
+            {/* <h1> {!text && 'not'}</h1> */}
             
             
 
